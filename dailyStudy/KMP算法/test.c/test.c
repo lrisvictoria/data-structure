7f4c8ea33bd64cda1f7c4da70ef22870883f1091
exploreranduin <a1249967801@163.com>
2022-09-12 23:33:59 +0800

    9.12 KMP code

---
+++ dailyStudy/KMP算法/test.c/test.c
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+//KMP
+/*
+str：主串
+sub：子串
+pos：开始匹配的位置
+*/
+
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+
+void GetNext(int* next, const char* sub)
+{
+	int lenSub = strlen(sub);
+	next[0] = -1;
+	next[1] = 0;
+	int i = 2;//开始位置
+	int k = 0;//前一项的k
+	
+	while (i < lenSub)
+	{
+		if ((k == -1) || sub[i - 1] == sub[k])
+		{
+			next[i] = k + 1;
+			i++;
+			k++;
+		}
+		else
+		{
+			k = next[k];
+		}
+	}
+}
+int KMP(const char* str, const char* sub, int pos)
+{
+	assert(str && sub);
+	int i = pos;//开始匹配的位置
+	int j = 0;//记录子串下标
+	int lenStr = strlen(str), lenSub = strlen(sub);
+
+	int* next = (int*)malloc(lenSub * sizeof(int));
+	assert(next);
+
+	GetNext(next, sub);
+
+	while (i < lenStr && j < lenSub)
+	{
+		if (j == -1 || str[i] == sub[j])
+		{
+			i++;
+			j++;
+		}
+		else
+		{
+			j = next[j];
+		}
+	}
+	free(next);
+	if (j >= lenSub)
+	{
+		return i - j;
+	}
+	return -1;
+}
+
+int main()
+{
+	printf("%d\n", KMP("ababcabcdabcde","abcd", 0));
+	return 0;
+}
