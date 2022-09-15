33790e42828426ced900d3c2b059c4c5cd2d8fa8
exploreranduin <a1249967801@163.com>
2022-09-15 20:56:05 +0800

    9.15 完整KMP

--- dailyStudy/KMP算法/test.c/test.c
+++ dailyStudy/KMP算法/test.c/test.c
@@ -11,10 +11,29 @@ pos：开始匹配的位置
 #include <string.h>
 #include <stdlib.h>
 
+void GetNextVal(int* next, const char* sub)
+{
+	int i = 2;
+	int lenSub = strlen(sub);
+	
+	while (i < lenSub)
+	{
+		if (next[i] == sub[next[i]])
+		{
+			next[i] = next[next[i]];
+		}
+		i++;
+	}
+}
 void GetNext(int* next, const char* sub)
 {
+	assert(next && sub);
 	int lenSub = strlen(sub);
 	next[0] = -1;
+	if (lenSub == 1)
+	{
+		return;
+	}
 	next[1] = 0;
 	int i = 2;//开始位置
 	int k = 0;//前一项的k
@@ -32,6 +51,8 @@ void GetNext(int* next, const char* sub)
 			k = next[k];
 		}
 	}
+
+	GetNextVal(next, sub);
 }
 int KMP(const char* str, const char* sub, int pos)
 {
@@ -68,5 +89,6 @@ int KMP(const char* str, const char* sub, int pos)
 int main()
 {
 	printf("%d\n", KMP("ababcabcdabcde","abcd", 0));
+	printf("%d\n", KMP("abc", "c", 0));
 	return 0;
 }
