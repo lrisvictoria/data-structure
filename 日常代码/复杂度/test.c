5b0fac480f83eaa242ef6164f5a1c9e7bf75f1b5
exploreranduin <a1249967801@163.com>
2022-09-25 21:44:22 +0800

    9.25 时间复杂度/空间复杂度

--- 日常代码/复杂度/test.c
+++ 日常代码/复杂度/test.c
@@ -160,3 +160,182 @@ const char* strchr(const char* str, int character);
 //认为时间复杂度是O(N)
 //相当于结果只会有惊喜，因为我们使用的是最坏情况
 
+//冒泡排序的时间复杂度
+//void BubbleSort(int* a, int n)
+//{
+//	assert(a);
+//	for (size_t end = n; end > 0; --end)
+//	{
+//		int exchange = 0;
+//		for (size_t i = 1; i < end; ++i)
+//		{
+//			if (a[i - 1] > a[i])
+//			{
+//				Swap(&a[i - 1], &a[i]);
+//				exchange = 1;
+//			}
+//		}
+//		if (exchange == 0)
+//			break;
+//	}
+//}
+//等差数列
+//N - 1
+//N - 2
+//N - 3
+//...
+//N*(N-1)/2
+//看阶数最高的一项
+//F(N) = O(N^2)
+
+//二分查找算法的时间复杂度？
+
+//int BinarySearch(int* a, int n, int x)
+//{
+//	assert(a);
+//	int begin = 0;
+//	int end = n - 1;
+//	while (begin < end)
+//	{
+//		int mid = begin + ((end - begin) >> 1);
+//		if (a[mid] < x)
+//			begin = mid + 1;
+//		else if (a[mid] > x)
+//			end = mid;
+//		else
+//			return mid;
+//	} 
+//	return -1;
+//}
+
+//算时间复杂度不能只看是几层循环，而要去看他的思想
+
+//时间复杂度：O(N) //err
+
+//O*(log2^N)//log 2底N
+
+//最好的情况：O(1)
+//最坏的情况：
+//反着推导
+//当查找完毕，把它当成一张纸 
+//当left == right时，这为1次
+//查找完成后每展开依次就是2次，
+//然后次数就是2^n，从最里面一层开始，每一层展开都*2
+//1*2*2*2..=N
+//2^X = N
+//X = log2^N
+
+//正向推导
+//分隔空间
+//N/2/2/2...=1
+//2^X = N 
+
+//时间复杂度O(log2^N)
+
+//二分查找算法是一个非常牛的算法
+//N个数中查找，大概查找次数
+//1000     10
+//100w     20
+//10亿     30
+
+//想象一下用二分查找，查找我们中国同胞
+//2^31约等于20亿
+//也就相当于我们最差情况下只要31次就能查找到这个人
+//缺点就是二分查找需要有序，排序的代价高，放到内存中也很大
+
+
+//递归计算阶乘的时间复杂度？
+
+//long long Factorial(size_t N)
+//{
+//	return N < 2 ? N : Factorial(N - 1) * N;
+//}
+
+//递归算法：递归次数*每次递归调用的次数
+//O(N)
+ 
+
+// 计算斐波那契递归Fibonacci的时间复杂度？
+//long long Fibonacci(size_t N)
+//{
+//	return N < 2 ? N : Fibonacci(N - 1) + Fibonacci(N - 2);
+//}
+
+//相当于细胞分裂的过程
+//假设每一层都是满的，则从2^0逐渐到2^n-1
+//Fib(N) =2^0 + 2^1 + 2^2 + ...+ 2^(N-1) - X
+//X是因为右边一些递归部分会提前结束
+//前部分和为2^N -1
+
+//当数据够大时，X远小于2^N
+//时间复杂度：0(2^N)
+
+
+
+//空间复杂度
+//空间复杂度算的是变量的个数
+//也是大O渐进表示法
+
+
+//void BubbleSort(int* a, int n)
+//{
+//	assert(a);
+//	for (size_t end = n; end > 0; --end)
+//	{
+//		int exchange = 0;
+//		for (size_t i = 1; i < end; ++i)
+//		{
+//			if (a[i - 1] > a[i])
+//			{
+//				Swap(&a[i - 1], &a[i]);
+//				exchange = 1;
+//			}
+//		}
+//		if (exchange == 0)
+//			break;
+//	}
+//}
+
+//O(1)
+//三个变量一个exchange，一个end，一个i，每次i空间销毁，下次还是使用这块空间
+
+//long long* Fibonacci(size_t n)
+//{
+//	if (n == 0)
+//		return NULL;
+//	long long* fibArray =
+//		(long long*)malloc((n + 1) * sizeof(long long));
+//	fibArray[0] = 0;
+//	fibArray[1] = 1; for (int i = 2; i <= n; ++i)
+//	{
+//		fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+//	}
+//	return fibArray;
+//}
+
+//空间复杂度O(N)
+
+//long long Factorial(size_t N)
+//{
+//	return N < 2 ? N : Factorial(N - 1) * N;
+//}
+//空间复杂度O(N)
+//递归的深度为N，建立了N个栈帧
+//每个递归的变量看做O(1)
+
+//斐波那契数列递归算法的空间复杂度
+long long Fibonacci(size_t N)
+{
+	return N < 2 ? N : Fibonacci(N - 1) + Fibonacci(N - 2);
+}
+//O(N)
+//空间是可以重复利用的，不累计的
+//时间是一去不复返的，累计的
+
+//递归的深度是n - 1，为O(N)
+//斐波那契额数列会先算Fib(N-1)再算Fib(N-2)
+//算Fib(N-1)会一条直线直接算下去，然后递归回去
+//当Fib(N-1)的递归调用完毕之后，调用另外一遍的递归
+//使用的是同一块空间，空间重复使用了
+
+//递归是一条路递归完了回去递归另外一条路
