67cdb2821bf6db7467d30515c98f6e14acf5bb36
exploreranduin <a1249967801@163.com>
2022-09-26 22:04:55 +0800

    9.26 消失的数字.4/轮转数组.3

--- 日常代码/oj训练1/test.c
+++ 日常代码/oj训练1/test.c
@@ -7,24 +7,24 @@
 
 //思路1：排序，看后一个数是不是前一个数+1
 //qsort 快排 ->时间复杂度O(N*log2^N) 不满足O(N)
-int cmp(const void* e1, const void* e2)
-{
-	return *(int*)e1 - *(int*)e2;
-}
-
-int missingNumber(int* nums, int numsSize)
-{
-	int ans = 0;
-	qsort(nums, numsSize, sizeof(int), cmp);
-	for (int i = 0; i < numsSize - 1; i++)
-	{
-		if ((nums[i + 1] - nums[i]) != 1)
-		{
-			return i + 1;
-		}
-	}
-	return ans;
-}
+//int cmp(const void* e1, const void* e2)
+//{
+//	return *(int*)e1 - *(int*)e2;
+//}
+//
+//int missingNumber(int* nums, int numsSize)
+//{
+//	int ans = 0;
+//	qsort(nums, numsSize, sizeof(int), cmp);
+//	for (int i = 0; i < numsSize - 1; i++)
+//	{
+//		if ((nums[i + 1] - nums[i]) != 1)
+//		{
+//			return i + 1;
+//		}
+//	}
+//	return ans;
+//}
 
 
 //思路2：0+1+2+3+...+n - (a[0] + a[1] + a[2]... + a[n - 1])
@@ -76,3 +76,59 @@ int missingNumber(int* nums, int numsSize)
 //    }
 //    return ans;
 //}
+
+
+//189. 轮转数组
+//思路1：暴力求解
+//qosrt排序后，判断前后两个值是否为1
+//void rotate(int* nums, int numsSize, int k)
+//{
+//    if (k >= numsSize)
+//    {
+//        k %= numsSize;
+//    }
+//    for (int i = 0; i < k; i++)
+//    {
+//        int tmp = nums[numsSize - 1];
+//        for (int j = numsSize - 1; j >= 1; j--)
+//        {
+//            nums[j] = nums[j - 1];
+//        }
+//        nums[0] = tmp;
+//    }
+//}
+
+//思路2：创建数组，以空间换时间
+//void rotate(int* nums, int numsSize, int k)
+//{
+//    int newArr[numsSize];
+//    for (int i = 0; i < numsSize; i++)
+//    {
+//        newArr[(i + k) % numsSize] = nums[i];
+//    }
+//    for (int i = 0; i < numsSize; i++)
+//    {
+//        nums[i] = newArr[i];
+//    }
+//}
+
+//思路3：三步翻转法
+void Reverse(int* nums, int left, int right)
+{
+    while (left < right)
+    {
+        int tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = tmp;
+        left++;
+        right--;
+    }
+}
+void rotate(int* nums, int numsSize, int k)
+{
+    if (k >= numsSize)
+        k %= numsSize;
+    Reverse(nums, 0, numsSize - k - 1);
+    Reverse(nums, numsSize - k, numsSize - 1);
+    Reverse(nums, 0, numsSize - 1);
+}
