85f4aa5bba2dc856c1c789883ddc90a5f3691480
exploreranduin <a1249967801@163.com>
2022-10-01 20:05:49 +0800

    10.1 消失的数字/轮转数组

--- 日常代码/SeqList/test.c
+++ 日常代码/SeqList/test.c
@@ -1,1 +1,68 @@
 #define _CRT_SECURE_NO_WARNINGS 1 
+
+#include "SeqList.h"
+
+void TestSeqList1()
+{
+	SL s1;
+	SeqListInit(&s1);
+	SeqListPushBack(&s1, 1);
+	SeqListPushBack(&s1, 2);
+	SeqListPushBack(&s1, 3);
+	SeqListPushBack(&s1, 4);
+	SeqListPushBack(&s1, 5);
+	SeqListPrint(&s1);
+	
+	SeqListPopBack(&s1);
+	SeqListPopBack(&s1);
+	SeqListPopBack(&s1);
+	SeqListPopBack(&s1);
+	SeqListPopBack(&s1);
+	SeqListPopBack(&s1);
+	SeqListPopBack(&s1);
+	SeqListPopBack(&s1);
+	SeqListPrint(&s1);
+
+	SeqListPushBack(&s1,6);
+	SeqListPushBack(&s1,7);
+
+	// 编译器也是人写的，总会有疏忽的时候
+	// 当越界时，可能会检查不出来
+	// 只有当销毁时，才会发现错误
+	SeqListDestory(&s1);
+}
+
+void TestSeqList2()
+{
+	SL s1;
+	SeqListInit(&s1);
+	SeqListPushBack(&s1, 1);
+	SeqListPushBack(&s1, 2);
+	SeqListPushBack(&s1, 3);
+	SeqListPushBack(&s1, 4);
+	SeqListPushBack(&s1, 5);
+	//SeqListPrint(&s1);
+
+	SeqListPushFront(&s1, 10);
+	SeqListPushFront(&s1, 20);
+	SeqListPushFront(&s1, 30);
+	SeqListPushFront(&s1, 40);// 程序会崩，越界了
+	SeqListPrint(&s1);
+
+	SeqListPopFront(&s1);
+	SeqListPopFront(&s1);
+	SeqListPopFront(&s1);
+	SeqListPopFront(&s1);
+	SeqListPrint(&s1);
+
+	SeqListDestory(&s1);
+
+}
+
+int main()
+{
+	//TestSeqList1();
+	TestSeqList2();
+
+	return 0;
+}
