3544f8b70f2d3532ab6a5a8a46e655dedc72d699
exploreranduin <a1249967801@163.com>
2022-10-08 22:45:54 +0800

    10.8 单链表可视化

--- 学校代码/SList/main.cpp
+++ 学校代码/SList/main.cpp
@@ -1,19 +1,71 @@
 #include <graphics.h>
-#include <stdio.h>
-
 #define DataType int
-
-typedef struct Node
+typedef struct Node 
 {
 	DataType data;
-	struct Node* next;
-}SLNode;
+	struct Node *next;
+} SLNode;
 
-SLNode* head = NULL;
-
-// 1. 
-
-int main()
+SLNode *head=NULL;
+//1
+void LinkInitiate(SLNode **head) 
 {
-	return 0;
+	*head = (SLNode *)malloc(sizeof(SLNode));
+	(*head)->data = -1;
+	(*head)->next =NULL;
+
+
+
+}
+//2
+int LinkInsertHead(SLNode *head,DataType x) 
+{
+	SLNode *s=(SLNode *)malloc(sizeof(SLNode));
+	s->data=x;
+	s->next =head->next;
+	head->next=s;
+	return 1;
+}
+//3
+void Show()
+{
+	cleardevice();
+	SLNode *p=head;
+	int i=0;
+	while(p!=NULL) 
+	{
+		rectangle(100+160*i,100,200+160*i,200);
+		xyprintf(100+160*i,100,"%d",p->data);
+		line(100+160*i,150,200+160*i,150);
+		xyprintf(100+160*i,160,"%x",p->next);
+		p=p->next;
+		i++;
+	}
+	for(int u=0; u<i-1; u++) 
+	{
+		line(220+160*u,150,250+160*u,150);//¼ýÍ·
+		line(230+160*u,120,250+160*u,150);
+		line(230+160*u,180,250+160*u,150);
+	}
+
+}
+int main() 
+{
+	initgraph(800, 600);
+	setcolor(BLUE);
+	setbkcolor(WHITE);
+	setbkmode(TRANSPARENT);
+	setrendermode(RENDER_MANUAL);
+	LinkInitiate(&head);
+	Show();
+	getch();
+	LinkInsertHead(head, 5);
+	Show();
+	getch();
+	LinkInsertHead(head, 5);
+	Show();
+	getch();
+	LinkInsertHead(head, 5);
+	Show();
+	getch();
 }
