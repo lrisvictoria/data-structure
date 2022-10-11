25001fa674234431a231a1db0e5752c0eb44203c
exploreranduin <a1249967801@163.com>
2022-10-11 09:09:54 +0800

    10.11 顺序栈完整实现

--- 日常代码/Stack/Stack.c
+++ 日常代码/Stack/Stack.c
@@ -13,19 +13,75 @@ void StackInit(ST* ps)
 }
 
 // 销毁栈
-void StackDestroy(ST* ps);
+void StackDestroy(ST* ps)
+{
+	assert(ps);
+	free(ps->a);
+	ps->a = NULL;
+	ps->capacity = 0;
+	ps->top = 0;
+}
 
 // 入栈
-void StackPush(ST* ps, STDataType x);
+void StackPush(ST* ps, STDataType x)
+{
+	assert(ps);
+
+	if (ps->top == ps->capacity)
+	{
+		int newcapacity = (ps->capacity == 0 ? 4 : ps->capacity * 2);
+
+		STDataType* tmp = (STDataType*)realloc(ps->a, sizeof(STDataType) * newcapacity);
+		if (tmp == NULL)
+		{
+			printf("realloc fail\n");
+			exit(-1);
+		}
+		else
+		{
+			ps->a = tmp;
+			ps->capacity = newcapacity;
+		}
+	}
+	
+	ps->a[ps->top] = x;
+	ps->top++;
+}
 
 // 出栈
-void StackPop(ST* ps);
+void StackPop(ST* ps)
+{
+	assert(ps);
+
+	assert(!StackEmpty(ps));
+	ps->top--;
+}
 
 // 取栈顶的数据
-STDataType StackTop(ST* ps);
+STDataType StackTop(ST* ps)
+{
+	assert(ps);
+	assert(!StackEmpty(ps));
+	
+	return ps->a[ps->top - 1];
+}
 
 // 计算栈的大小
-int StackSize(ST* ps);
+int StackSize(ST* ps)
+{
+	assert(ps);
+
+	return ps->top;
+}
 
 // 判断栈中是否有元素
-bool StackEmpty(ST* ps);
+bool StackEmpty(ST* ps)
+{
+	assert(ps);
+
+	return ps->top == 0;
+	// 栈中无元素
+	// 返回真，！StackEmpty为假
+	// 栈中有元素
+	// 返回假，! StackEmpty为真
+}
--- 日常代码/Stack/test.c
+++ 日常代码/Stack/test.c
@@ -12,12 +12,42 @@ void TestStack1()
 	StackPush(&st, 4);
 	StackPush(&st, 5);
 
-
+	StackPop(&st);
+	StackPop(&st);
+	StackPop(&st);
+	StackPop(&st);
+	StackPop(&st);
+	//StackPop(&st);
 
 }
 
+void TestStack2()
+{
+	ST st;
+	StackInit(&st);
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	
+	printf("%d ", StackTop(&st));// ³öÕ»2
+	StackPop(&st);
+
+	printf("%d ", StackTop(&st));// ³öÕ»1
+	StackPop(&st);
+
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+	StackPush(&st, 5);
+
+	while (!StackEmpty(&st))
+	{
+		printf("%d ", StackTop(&st));
+		StackPop(&st);
+	}
+
+}
 int main()
 {
-	TestStack1();
+	//TestStack1();
+	TestStack2();
 	return 0;
 }
