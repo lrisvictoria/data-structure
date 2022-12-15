28b0e07f47526968117fe55cd529167ef48593c5
exploreranduin <a1249967801@163.com>
2022-12-15 21:54:16 +0800

    12.15 add

--- 上机代码/Sort/Sort.c
+++ 上机代码/Sort/Sort.c
@@ -13,15 +13,15 @@ void PrintArray(int* a, int n)
 
 void InsertSort(int* a, int n)
 {
-	// < n 会越界
 	for (int i = 0; i < n - 1; i++)
 	{
 		// 单趟
 		int end = i;
 		int tmp = a[end + 1];
+
 		while (end >= 0)
 		{
-			if (tmp < a[end])
+			if (a[end] > tmp)
 			{
 				a[end + 1] = a[end];
 				end--;
@@ -31,6 +31,8 @@ void InsertSort(int* a, int n)
 				break;
 			}
 		}
+
+		// 填到 end + 1 位置
 		a[end + 1] = tmp;
 	}
 }
@@ -72,15 +74,17 @@ void ShellSort(int* a, int n)
 	//}
 
 	// 三层循环
+	
 	while (gap > 1)
 	{
 		gap = gap / 3 + 1;
-		// 进行多组并排
+
+		// 三组并排
 		for (int i = 0; i < n - gap; i++)
 		{
-			// 对于单个元素
 			int end = i;
 			int tmp = a[end + gap];
+
 			while (end >= 0)
 			{
 				if (tmp < a[end])
@@ -93,9 +97,11 @@ void ShellSort(int* a, int n)
 					break;
 				}
 			}
+
 			a[end + gap] = tmp;
 		}
 	}
+	
 }
 
 void Swap(int* p1, int* p2)
@@ -148,41 +154,52 @@ void HeapSort(int* a, int n)
 
 void SelectSort(int* a, int n)
 {
-	int begin = 0, end = n - 1;
+	/*int begin = 0, end = n - 1;
 
 	while (begin < end)
 	{
-		// mini 和 maxi 需要一直更新
-		// 每次从 begin 开始，筛选出最大值的下标和最小值的下标
 		int mini = begin, maxi = begin;
+
 		for (int i = begin + 1; i <= end; i++)
 		{
-			// 如果 i 处值 小于 begin(最小值位置) 所在处值，更新 mini
 			if (a[i] < a[mini])
 			{
 				mini = i;
 			}
 
-			// 如果 i 处值 大于 end(最大值位置) 所在处值，更新maxi
 			if (a[i] > a[maxi])
 			{
 				maxi = i;
 			}
 		}
 
-		// 分别交换 begin 和 mini 的值 / end 和 maxi 的 值
-		// 特殊情况特殊处理
-		
 		Swap(&a[begin], &a[mini]);
 		if (begin == maxi)
 		{
 			maxi = mini;
 		}
 		Swap(&a[end], &a[maxi]);
-
-		// 更新 begin 和 end
 		begin++;
 		end--;
+	}*/
+
+	// 选 1 个数
+	int begin = 0;
+
+	while (begin < n)
+	{
+		int mini = begin;
+
+		for (int i = begin + 1; i < n; i++)
+		{
+			if (a[i] < a[mini])
+			{
+				mini = i;
+			}
+		}
+
+		Swap(&a[begin], &a[mini]);
+		begin++;
 	}
 }
 
@@ -190,22 +207,18 @@ void BubbleSort(int* a, int n)
 {
 	for (int j = 0; j < n - 1; j++)
 	{
-		int exchange = 0;
-		// 单趟
-		for (int i = 0; i < n - 1 - j; i++)
+		int flag = 1;
+		for (int i = 0; i < n - j - 1; i++)
 		{
 			if (a[i] > a[i + 1])
 			{
 				Swap(&a[i], &a[i + 1]);
-				exchange = 1;
+				flag = 0;
 			}
-			
-			// 如果一趟排序中第一次没有交换，说明本趟有序
-			// 直接跳转到下一趟
-			if (exchange == 0)
-			{
-				break;
-			}
+		}
+		if (flag)
+		{
+			break;
 		}
 	}
 }
@@ -213,43 +226,60 @@ void BubbleSort(int* a, int n)
 // 快排
 void QuickSort(int* a, int begin, int end)
 {
-	// 如果 l > r 那么没必要递归，直接返回
 	if (begin >= end)
 	{
 		return;
 	}
 
-	int left = begin;
-	int right = end;
-	int keyi = left;
+	int left = begin, right = end;
 
-	// 一次没必要递归
+	int key = left;
+
 	while (left < right)
 	{
-		// 防止越界，右边大于等于 keyi 值就 --
-		while (left < right && a[right] >= a[keyi])
+		// 右边先走
+		// 两个条件一个防止跑出去，找大于 a[key] 的值
+		while (left < right && a[right] >= a[key])
 		{
 			right--;
 		}
 
-		// 防止越界，左边小于等于 keyi 值就 ++
-		while (left < right && a[left] <= a[keyi])
+		while (left < right && a[left] <= a[key])
 		{
 			left++;
 		}
-		
-		// 此时停下来，一定是交换位置的地方
+
 		Swap(&a[left], &a[right]);
 	}
 
-	// 一次完后，交换 left 和 keyi 下标对应的值
-	// L 和 R 相遇的位置一定小于 keyi
-	Swap(&a[left], &a[keyi]);
-	
-	// left 变成新的 keyi
-	keyi = left;
+	Swap(&a[left], &a[key]);
 
-	// 分别递归左边和右边
-	QuickSort(a, begin, keyi - 1);
-	QuickSort(a, keyi + 1, end);
+	// 改变 key 的位置
+	key = left;
+
+	// 递归左右区间
+	QuickSort(a, begin, key - 1);
+	QuickSort(a, key + 1, end);
+}
+
+// 归并排序非递归
+void MergeSortNonR(int* a, int n)
+{
+	int* tmp = (int*)malloc(sizeof(int) * n);
+	if (tmp == NULL)
+	{
+		perror("malloc fail");
+		exit(-1);
+	}
+
+	int rangeN = 1;
+	for (int i = 0; i < n; i += rangeN)
+	{
+		int begin1 = i, end1 = i + rangeN - 1;
+		int begin2 = i + rangeN, end2 = i + 2 * rangeN - 1;
+
+		int j = i;
+		
+
+	}
 }
